9df628446cba46b4b220b69820fc64b9ee9628da
tobe09 <tobe09@users.noreply.github.com>
2018-02-21 16:10:26 +0100

    Files Added
    Files initially added to the repository

---
+++ TobeMvcPractise/Models/HomeViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TobeMvcPractise.Models
+{
+    public class HomeIndexModel
+    {
+        public string Name { get; set; } = "Tobenna";
+        public string Information { get; set; } = "I am a BadAs* developer";
+    }
+}
