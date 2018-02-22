4024a2df0455ef3733512cc15bf0c7de9a11635e
tobe09 <tobe09@users.noreply.github.com>
2018-02-22 08:30:22 +0100

    Update

--- TobeMvcPractise/App_Start/RouteConfig.cs
+++ TobeMvcPractise/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace TobeMvcPractise
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "Testing", 
+                "TestRoute/{action}/{name}", 
+                new { controller = "TestRoute", action = "List", name = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ TobeMvcPractise/Controllers/TestRouteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TobeMvcPractise.Controllers
+{
+    public class TestRouteController : Controller
+    {
+        public string List(string name)
+        {
+            string str = @"Hello " + name + ".<br/><strong>From TestRoute</strong>";
+
+            return str;
+        }
+    }
+}
